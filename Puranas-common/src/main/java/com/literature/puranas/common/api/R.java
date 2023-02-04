943ea6c57b10366a45d9b319826cb62c0b9186bf
Owen <Owen.DK.Ou@pccw.com>
2023-02-04 10:48:35 +0800

    统一返回

--- Puranas-common/src/main/java/com/literature/puranas/common/api/R.java
+++ Puranas-common/src/main/java/com/literature/puranas/common/api/R.java
@@ -1,7 +1,50 @@
 package com.literature.puranas.common.api;
 
+import com.literature.puranas.common.constant.CommonConstants;
+import lombok.*;
+import lombok.experimental.Accessors;
+
+import java.io.Serial;
 import java.io.Serializable;
 
-public class R implements Serializable {
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class R<T> implements Serializable {
+    @Serial
     private static final long serialVersionUID = 1L;
+
+    @Getter
+    @Setter
+    private int code;
+
+    @Getter
+    @Setter
+    private String msg;
+
+    @Getter
+    @Setter
+    private T data;
+
+    public static <T> R<T> restReturn(int code, T data, String msg) {
+        R<T> result = new R<>();
+        result.setCode(code);
+        result.setData(data);
+        result.setMsg(msg);
+        return result;
+    }
+
+    public static <T> R<T> data() {
+        return restReturn(ReturnCode.SUCCESS.getCode(), null, CommonConstants.DefaultReturnMsg);
+    }
+
+    public static <T> R<T> data(int code,T data,String msg) {
+        return restReturn(code, data, msg);
+    }
+
+    public static <T> R<T> success() {
+        return restReturn(ReturnCode.SUCCESS.getCode(), null, null);
+    }
+
 }
